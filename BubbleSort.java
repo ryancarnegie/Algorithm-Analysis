377291a6b8541a4164ffc0353421229407fcb2d4
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-02-05 00:53:00 -0800

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,11 @@
+for i = 1:n,
+    swapped = false
+    for j = n:i+1,
+        if a[j] < a[j-1],
+            swap a[j,j-1]
+            swapped = true
+    → invariant: a[1..i] in final position
+    break if not swapped
+end
+
+
