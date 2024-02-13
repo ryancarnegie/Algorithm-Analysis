55cafd85609031eb555432bfdb4793aa1ae00fa3
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-02-13 01:09:54 -0800

    Update Divide&Conquer.java

--- Divide&Conquer.java
+++ Divide&Conquer.java
@@ -1,1 +1,23 @@
+import java.util.Arrays;
 
+public class Divide&Conquer {
+
+	static int findMax(int a[], int left, int right)
+	{
+		int middle;
+		if (left == right) {
+			return a[left];
+		}
+		middle = (left + right) / 2;
+		int findLeft = findMax(a, left, middle);
+		int findRight = findMax(a, middle + 1, right);
+		System.out.println(Math.max(findLeft, findRight));
+		return Math.max(findLeft, findRight);	
+	}
+
+	public static void main(String[]args)
+	{
+		int a[] = {2,5,8,3,6,9,1,6,5,11};
+		int b[] = {3,2,7,6,10};
+		System.out.printf("Q1) findMax\nMaximum element: position %d\n", findMax(a, 0, a.length - 1) + 1);
+}
