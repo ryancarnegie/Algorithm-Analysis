59f5ca0d8b323187b0a175df3eb74e546dc7b83a
ryan105 <ryan.carnegie@gmail.com>
2024-02-13 00:58:31 -0800

    divide and conquer update

--- MergeSort.java
+++ MergeSort.java
@@ -5,28 +5,39 @@ public class MergeSort {
 
 	static int findMax(int a[], int left, int right)
 	{
-		// Implement here
-        
+		int middle;
+
+		if (left == right) {
+			return a[left];
+		}
+
+		middle = (left + right) / 2;
+		int findLeft = findMax(a, left, middle);
+		int findRight = findMax(a, middle + 1, right);
+		System.out.println(Math.max(findLeft, findRight));
+		return Math.max(findLeft, findRight);	
+		
 	}
 
 	
-	static int[] mergeSort(int[] A) {
-		if (A.length > 1) {
+	// static int[] mergeSort(int[] A) {
+	// 	if (A.length > 1) {
 		
-			// Implement here
-			merge(B, C, A);
-		}
-		return A;
-	}
+	// 		// Implement here
+	// 		merge(B, C, A);
+	// 	}
+	// 	return A;
+	// }
 	
 	static void merge(int[] B, int[] C, int[] A) {
 		// Implement here
+
 	}
 
 	public static void main(String[]args)
 	{
-		int a[] = {2,5,8,3,6,9,1,6,5};
-		int b[] = {3,2,7,6,8};
+		int a[] = {2,5,8,3,6,9,1,6,5,11};
+		int b[] = {3,2,7,6,10};
 		System.out.printf("Q1) findMax\nMaximum element: position %d\n", findMax(a, 0, a.length - 1) + 1);
 
 		System.out.print("Q2) mergeSort\n");
@@ -35,7 +46,7 @@ public class MergeSort {
 		for(int i = 0; i < b.length; i++)
 			System.out.print(b[i] + ", ");
 		
-		mergeSort(b);
+		//mergeSort(b);
 		
 		System.out.println("\nAfter sort:");
 		
