aba74270d4c063459b6b229030e138845e667f30
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-02-19 23:24:43 -0800

    Update Transform&Conquer.java

--- Transform&Conquer.java
+++ Transform&Conquer.java
@@ -1,1 +1,23 @@
 
+
+Transform & Conquer
+
+1. Instance simplification
+  - checking element uniqueness in an array
+  - computing a node
+2. Representation change
+  - how to implement a heap
+  - insert and delete frm a heap
+  - construct a heap
+use heap sort
+
+Presort uniqueness in an array(A[0..n-1])
+  for i <- to n-2 do
+    if A[i] = A[i + 1] return false
+  return true
+
+Stage 1: sort by efficient sorting algorithm (mergesort) O(n logn)
+Stage 2: scan array to check pairs of adjacent elements  O(n)
+actual overall big O is drop lowest terms = O(n + n log n) = O(nlogn)
+
+  
