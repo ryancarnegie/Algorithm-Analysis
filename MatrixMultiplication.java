e34934f21c27329df9ba9b2ee5b02018fbc21640
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-02-21 23:20:22 -0800

    Update MatrixMultiplication.java

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -7,18 +7,17 @@ for i <- 0 to n - 1 do
 return C
 
 
-
- int A[][] = {{1, 2, 3}, {4, 5, 6}};
-        int B[][] = {{7, 2, 1}, {5, 2, 1}};
-        int C[][] = new int[2][10];
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                C[i][j] = 0;
-                for (int k = 0; k < C.length; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
-                }
-                System.out.println(C[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+int A[][] = {{1, 2, 3}, {4, 5, 6}};
+      int B[][] = {{7, 2, 1}, {5, 2, 1}};
+      int C[][] = new int[2][10];
+      for (int i = 0; i < A.length; i++) {
+          for (int j = 0; j < B.length; j++) {
+              C[i][j] = 0;
+              for (int k = 0; k < C.length; k++) {
+                  C[i][j] += A[i][k] * B[k][j];
+              }
+              System.out.println(C[i][j] + " ");
+          }
+          System.out.println();
+      }
+  }
