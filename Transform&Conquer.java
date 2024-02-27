ece525783493b3e481896489f3cd68234a5451f4
Ryan <58387121+ryancarnegie@users.noreply.github.com>
2024-02-26 18:09:35 -0800

    Update Transform&Conquer.java

--- Transform&Conquer.java
+++ Transform&Conquer.java
@@ -19,3 +19,10 @@ Stage 2: scan array to check pairs of adjacent elements  O(n)
 actual overall big O is drop lowest terms = O(n + n log n) = O(nlogn)
 
   
+Given an array, let's transform it into a new data structure. The big O's of a heap - we can insert with O(logn), 
+and we can delete with O(logn). These are much faster than O(n). Inserting with n for a million elements would be
+a million steps. Inserting with n for a heap is logn - it will take 23 steps.
+
+What is a heap?
+A heap is a binary tree. A binary tree means that for each node it has 0 to 2 children. A heap is a binary tree filled
+on all levels, filled from left to right, except the last level. Parents are greater or equal than its children.
